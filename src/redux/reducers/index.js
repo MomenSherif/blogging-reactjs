a32b4663af6dd0a3ce5eb2508ae718bcd5350652
MomenSherif <momensherif.2019@gmail.com>
2020-05-09 03:09:18 +0200

    🔥 REMOVE: Blogs Reducer

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -3,12 +3,10 @@ import { combineReducers } from 'redux';
 // Import All Reducers
 import authenticationReducer from './authentication';
 import statusReducer from './status';
-import blogsReducer from './blogs';
 
 const rootReducer = combineReducers({
   auth: authenticationReducer,
   status: statusReducer,
-  blogs: blogsReducer,
 });
 
 export default rootReducer;
