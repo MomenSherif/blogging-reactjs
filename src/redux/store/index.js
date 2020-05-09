1d260cc3a788ea277d448c5cdfdabf376c759c8f
MomenSherif <momensherif.2019@gmail.com>
2020-05-09 14:41:42 +0200

    🔒 SECURITY: Redux Dev Tool in Development Mode Only

--- src/redux/store/index.js
+++ src/redux/store/index.js
@@ -2,7 +2,10 @@ import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import rootRucer from '../reducers';
 
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const composeEnhancers =
+  process.env.NODE_ENV == 'development'
+    ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+    : compose;
 
 const store = createStore(rootRucer, composeEnhancers(applyMiddleware(thunk)));
 
